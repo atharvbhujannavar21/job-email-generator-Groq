f97288f44216b44a3e30baccb86f7c58b05085e6
atharvbhujannavar21 <atharvbhujannavar21@gmail.com>
2025-04-07 23:17:54 +0530

    added streamlit ui

---
+++ app/chains.py
@@ -0,0 +1,70 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama3-8b-8192")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are an expert cold email copywriter.
+
+Generate a professional, concise cold email from Atharv Bhujannavar, a B.Tech graduate, who is reaching out to potential clients or companies. The goal is to introduce himself, express interest in collaborating or offering value through his skills, and initiate a conversation.
+
+Include:
+
+A warm, personalized greeting
+
+A short introduction of Atharv (as a recent B.Tech graduate)
+
+A clear value proposition or reason for reaching out
+
+A call to action (e.g., a meeting, call, or reply)
+
+Keep the tone polite, confident, and professional
+
+Output a subject line and the full email body.Also add the most relevant ones from the following links to showcase my portfolio: {link_list}
+            Remember you are atharv bhujannavar. 
+            Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
